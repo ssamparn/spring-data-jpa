0a2dd65647044857aa096b63614b03a4cba27997
samantrs <https://samantrs@git.rabobank.nl>
2023-04-09 12:11:25 +0200

    spring jdbc template - first commit

---
+++ sdjpa-jdbc-template/src/main/java/com/spring/data/jpa/sdjpajdbctemplate/SpringDataJpaJdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.spring.data.jpa.sdjpajdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaJdbcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaJdbcTemplateApplication.class, args);
+	}
+
+}
---
+++ sdjpa-jdbc-template/src/test/java/com/spring/data/jpa/sdjpajdbctemplate/SpringDataJpaJdbcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.data.jpa.sdjpajdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaJdbcTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
