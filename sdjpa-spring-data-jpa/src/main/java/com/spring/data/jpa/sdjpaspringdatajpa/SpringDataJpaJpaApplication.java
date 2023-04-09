301ad588c960174518552016701fbd0c3fbb77a1
samantrs <https://samantrs@git.rabobank.nl>
2023-04-09 19:03:28 +0200

    sdjpa JPA initial project added

---
+++ sdjpa-spring-data-jpa/src/main/java/com/spring/data/jpa/sdjpaspringdatajpa/SpringDataJpaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.data.jpa.sdjpaspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaJpaApplication.class, args);
+	}
+
+}
---
+++ sdjpa-spring-data-jpa/src/test/java/com/spring/data/jpa/sdjpaspringdatajpa/SpringDataJpaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.data.jpa.sdjpaspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
